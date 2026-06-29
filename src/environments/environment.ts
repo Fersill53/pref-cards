fe56066a462a968daf7656505e77664083618038
Fernando Siller <Fersill53@outlook.com>
2026-06-29 11:42:46 -0500

    fix: commit environment.ts and un-ignore environments folder for Render build

---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    supabaseUrl: 'https://ncspblgnwjgodakrvmrw.supabase.co',
+    supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im5jc3BibGdud2pnb2Rha3J2bXJ3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODIxNDkyMTgsImV4cCI6MjA5NzcyNTIxOH0.Pn0ygVG4U3YwKVG_ad1xsCGBqY_-L5E091LYplU5mjg'
+};
